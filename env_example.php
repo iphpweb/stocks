ae9fd11326a74c8b57cb5dcc1e3b6ae167cd382a
Evgeny <citizenpe@gmail.com>
2019-08-26 21:47:34 +0400

    env file, styles, extra columns

---
+++ env_example.php
@@ -0,0 +1,16 @@
+<?php
+// dev || test || prod
+define('APP_ENV', '');
+
+// dev-env localhost at BANKA
+define('DB_HOST', 'localhost');
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASS', '');
+
+// global project vars
+define('DOCUMENT_ROOT', $_SERVER['DOCUMENT_ROOT']);
+define('SITE_TEMPLATE_PATH', '');
+
+define('SECRET_IEX_KEY', '');
+define('PUBLIC_IEX_KEY', '');
--- index.php
+++ index.php
@@ -15,14 +15,15 @@ $num = $stmt->rowCount();
 ?>
 
 <div class="table-responsive">
-    <table class='table table-striped table-hover table-bordered index-stocks'>
+    <table class='table table-sm table-striped table-hover table-bordered index-stocks'>
         <thead class="thead-dark">
             <tr>
                 <th>Quote name</th>
                 <th>Amount</th>
-                <th>Price average</th>
-                <th>Price current</th>
-                <th>Delta</th>
+                <th>average Price</th>
+                <th>current Price</th>
+                <th>Delta, $</th>
+                <th>Delta, %</th>
             </tr>
         </thead>
         <tbody>
@@ -33,26 +34,28 @@ $num = $stmt->rowCount();
                 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
                     extract($row);
 
-                    //$quote_icon = $stocksAPI::getLogo($quote_name);
-                    $price_cur = 0; //$stocksAPI::getQuote($quote_name);
-                    $delta = $amount * ($price_cur - $price_avg);
-                    $class = 'table-' . ($delta > 0 ? 'success' : 'danger');
+                    $quote_icon = $stocksAPI::getLogo($quote_name);
+                    $price_cur = $stocksAPI::getQuote($quote_name);
+                    $delta_money = round($amount * ($price_cur - $price_avg), 2);
+                    $delta_prcnt = round($delta_money*100/($price_avg * $amount), 2);
+                    $class = 'table-' . ($delta_money > 0 ? 'success' : 'danger');
 
                     echo "<tr class='$class' data-thisrecordid='{$id}'>";
                         echo "<td><img src='{$quote_icon['url']}'> {$quote_name}</td>";
                         echo "<td>{$amount}</td>";
                         echo "<td>{$price_avg}</td>";
                         echo "<td>{$price_cur}</td>";
-                        echo "<td>{$delta}</td>";
+                        echo "<td>{$delta_money} $</td>";
+                        echo "<td>{$delta_prcnt} %</td>";
                     echo "</tr>";
 
-                    $total_delta += $delta;
+                    $total_delta += $delta_money;
                 }
 
                 $total_tr_class = 'bg-' . ($total_delta > 0 ? 'success' : 'danger');
                 $current_date = date('H:i', time()) . ' on ' . date('dS M, Y ', time());
                 echo "<tr class='{$total_tr_class}'>
-                        <td colspan='4'>Balance as of {$current_date}:</td>
+                        <td colspan='5'>Balance as of {$current_date}:</td>
                         <td>{$total_delta}</td>
                      </tr>";
             } else {
