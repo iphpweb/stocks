bb6cc2848c19ccc3a8c2f2e58e4929f4a4be0cdb
Evgeny Papilin <papilin.es@academia-moscow.ru>
2020-02-13 13:00:20 +0300

    update .env_example (avoid notice)

--- env_example.php
+++ env_example.php
@@ -1,16 +1,14 @@
 <?php
 // dev || test || prod
-define('APP_ENV', '');
+if (!defined('APP_ENV')) define('APP_ENV', '');
 
-// dev-env localhost at BANKA
-define('DB_HOST', 'localhost');
-define('DB_NAME', '');
-define('DB_USER', '');
-define('DB_PASS', '');
+if (!defined('DOCUMENT_ROOT')) define('DOCUMENT_ROOT', $_SERVER['DOCUMENT_ROOT']);
+if (!defined('SITE_TEMPLATE_PATH')) define('SITE_TEMPLATE_PATH', '');
 
-// global project vars
-define('DOCUMENT_ROOT', $_SERVER['DOCUMENT_ROOT']);
-define('SITE_TEMPLATE_PATH', '');
+if (!defined('DB_HOST')) define('DB_HOST', '');
+if (!defined('DB_NAME')) define('DB_NAME', '');
+if (!defined('DB_USER')) define('DB_USER', '');
+if (!defined('DB_PASS')) define('DB_PASS', '');
 
-define('SECRET_IEX_KEY', '');
-define('PUBLIC_IEX_KEY', '');
+if (!defined('SECRET_IEX_KEY')) define('SECRET_IEX_KEY', '');
+if (!defined('PUBLIC_IEX_KEY')) define('PUBLIC_IEX_KEY', '');
